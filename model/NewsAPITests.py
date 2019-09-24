249ac8c8411246b63f7244b8b5863bf2ba4c4aa7
Jacqueline Alves <jacque.alves.b@gmail.com>
2019-09-24 09:55:34 -0300

    Saving news table as csv file

--- model/NewsAPITests.py
+++ model/NewsAPITests.py
@@ -3,12 +3,18 @@ import pandas
 import json
 
 url = ('https://newsapi.org/v2/top-headlines?''country=br&''apiKey=f68f6e296da64d9397688593270fe001')
-response = requests.get(url)
+response = requests.get(url) # make request to NewsAPI
 
-df = pandas.read_json(json.dumps(response.json()['articles']), orient='records')
-columns = ['author','source','description','urlToImage']
-df = df.drop(columns, axis=1)
-categories = ['esporte'] * df.shape[0]
-df.insert(4, 'category', categories, True) 
-df.to_csv('/Users/manuellavalenca/Downloads/tabelinha.csv',index=False, sep='\t', enconding = 'utf-8')
-print df
+df = pandas.read_json(json.dumps(response.json()['articles']), orient='records') # gets the articles returned
+
+columns = ['author','source','description','urlToImage'] # unecessary columns to be removed
+df = df.drop(columns, axis=1) # remove unecessary columns
+
+categories = ['esporte'] * df.shape[0] # create array of categories to add new column to data frama (test)
+df.insert(4, 'category', categories, True)  # insert category column to data frame
+
+df = df[['title', 'content', 'publishedAt', 'category', 'url']] # reorganizing columns 
+
+df.to_csv('news-table.csv', sep='\t') # saving data frama to csv
+
+print(df)
