61d56bb7686f0dbd441dfade3b41b17bde208b62
manuzinha <manuella.valenca@ufpe.br>
2019-09-24 09:36:42 -0300

    Adding category column

--- model/NewsAPITests.py
+++ model/NewsAPITests.py
@@ -8,5 +8,7 @@ response = requests.get(url)
 df = pandas.read_json(json.dumps(response.json()['articles']), orient='records')
 columns = ['author','source','description','urlToImage']
 df = df.drop(columns, axis=1)
-
+categories = ['esporte'] * df.shape[0]
+df.insert(4, 'category', categories, True) 
+df.to_csv('/Users/manuellavalenca/Downloads/tabelinha.csv',index=False, sep='\t', enconding = 'utf-8')
 print df
