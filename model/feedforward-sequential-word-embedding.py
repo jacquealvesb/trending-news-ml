adf9233f079d1abd113e906fc2cedf50faf66bd1
Jacqueline Alves <jacque.alves.b@gmail.com>
2019-10-01 08:59:12 -0300

    Slipping the test and training data per category

--- model/feedforward-sequential-word-embedding.py
+++ model/feedforward-sequential-word-embedding.py
@@ -5,25 +5,47 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
 from sklearn.preprocessing import LabelEncoder
 from keras import layers
+from sklearn.utils import shuffle
+from math import ceil
+import numpy as np
 
 df = pandas.read_csv('pre-processed-text-dataset.csv', sep='\t')
 df.head()
 
-sentences = df['text'].values.astype('U')
-y = df['category'].values
 encoder = LabelEncoder()
-y = encoder.fit_transform(y) # transform category label from string to number
+df['category'] = encoder.fit_transform(df['category'].values) # transform category label from string to number
 
-sentences_train, sentences_test, y_train, y_test = train_test_split(sentences, y, test_size=0.2, shuffle=True) # splitting test and train data
+sentences_train = []
+sentences_test = []
+y_train = []
+y_test = []
+
+for category in set(df['category'].values):
+    category_table = df.loc[df['category'] == category]
+    category_sentences = category_table['text'].values.astype('U')
+    category_y = category_table['category'].values
+
+    category_sentences_train, category_sentences_test, category_y_train, category_y_test = train_test_split(category_sentences, category_y, test_size=0.2, shuffle=True) # splitting test and train data
+
+    sentences_train += list(category_sentences_train)
+    sentences_test += list(category_sentences_test)
+    y_train += list(category_y_train)
+    y_test += list(category_y_test)
+
+sentences_train, y_train = shuffle(sentences_train, y_train, random_state = 0)
+sentences_test, y_test = shuffle(sentences_test, y_test, random_state = 0)
 
 tokenizer = Tokenizer(num_words = 5000)
 tokenizer.fit_on_texts(sentences_train)
 x_train = tokenizer.texts_to_sequences(sentences_train)
 x_test = tokenizer.texts_to_sequences(sentences_test)
-vocab_size = len(tokenizer.word_index) + 1 
-print(sentences_train[2],x_train[2])
 
-maxlen = 5453 # size of the largest text
+# words = list(tokenizer.word_index.keys())
+# print(words[:100])
+
+vocab_size = len(tokenizer.word_index) + 1 
+
+maxlen = 152 # size of the largest text
 # for sentence in x_train:
 #     if len(sentence) > maxlen:
 #         maxlen = len(sentence)
@@ -31,7 +53,13 @@ maxlen = 5453 # size of the largest text
 x_train = pad_sequences(x_train, padding = 'post', maxlen=maxlen)
 x_test = pad_sequences(x_test, padding='post', maxlen=maxlen)
 
-embedding_dim = int(maxlen/2)
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+x_test = np.array(x_test)
+y_test = np.array(y_test)
+
+embedding_dim = ceil(maxlen/2)
 model = Sequential()
 model.add(layers.Embedding(input_dim = vocab_size, output_dim = embedding_dim, input_length = maxlen))
 model.add(layers.GlobalMaxPool1D())
@@ -40,7 +68,11 @@ model.add(layers.Dense(1,activation='sigmoid')) # add output layer
 model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 model.summary()
 
-history = model.fit(x_train, y_train, epochs = 20, verbose = False, validation_data =(x_test, y_test), batch_size = 10) # fit model into 
+history = model.fit(x_train, y_train,
+                    epochs=20,
+                    verbose=False,
+                    validation_data=(x_test, y_test),
+                    batch_size=10)
 loss, accuracy = model.evaluate(x_train, y_train, verbose = False)
 print("Training Accuracy: {:.4f}".format(accuracy))
 loss, accuracy = model.evaluate(x_test, y_test, verbose=False)
