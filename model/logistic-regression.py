bebbb4ebe68fb3581a9b150a90871fed903e1586
manuzinha <manuella.valenca@ufpe.br>
2019-10-07 10:50:15 -0300

    CoreML conversion

--- model/logistic-regression.py
+++ model/logistic-regression.py
@@ -2,6 +2,7 @@ import pandas
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
+import coremltools
 
 df = pandas.read_csv('pre-processed-text-dataset.csv', sep='\t')
 df.head()
@@ -18,10 +19,22 @@ x_train = vectorizer.transform(sentences_train) # creates feature vector of trai
 x_test = vectorizer.transform(sentences_test) # creates feature vector of test data
 
 classifier = LogisticRegression()
+classifier.multi_class = "ovr"
 classifier.fit(x_train, y_train) # fit the model according to train data
 score = classifier.score(x_test, y_test) # gets the mean accuracy of the test data and labels
 
 print(score)
+print(type(classifier))
+input_feature = 'newsText'
+output_feature = 'category'
+coreml_model = coremltools.converters.sklearn.convert(classifier, input_feature, output_feature)
 
+coreml_model.author = 'Jacque Alves e Manuella Valença'
+coreml_model.license = 'MIT License'
+coreml_model.short_description = 'Brazilian news category prediction.'
+coreml_model.input_description['newsText'] = 'An article text as a tokenized array.'
+coreml_model.output_description['category'] = 'Most scored category'
+
+coreml_model.save('BrNewsCategoryV2.mlmodel')
 
 
