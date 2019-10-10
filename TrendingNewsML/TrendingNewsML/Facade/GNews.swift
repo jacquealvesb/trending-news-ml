4ca82f5f519a87e56aac3b31da8ee53a8e385d96
Jacqueline Alves <jacque.alves.b@gmail.com>
2019-10-10 13:54:00 -0300

    Add git ignore to ignore file that contains the API key

---
+++ TrendingNewsML/TrendingNewsML/Facade/GNews.swift
@@ -0,0 +1,9 @@
+//
+//  GNews.swift
+//  TrendingNewsML
+//
+//  Created by Jacqueline Alves on 10/10/19.
+//  Copyright © 2019 maqueline. All rights reserved.
+//
+
+import Foundation
---
+++ TrendingNewsML/TrendingNewsML/Model/News.swift
@@ -0,0 +1,13 @@
+//
+//  Article.swift
+//  TrendingNewsML
+//
+//  Created by Jacqueline Alves on 10/10/19.
+//  Copyright © 2019 maqueline. All rights reserved.
+//
+
+import Foundation
+
+class News {
+    
+}
