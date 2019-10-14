68cf3e4b3e86934431645e32b0226c187a65bfe8
Jacqueline Alves <jacque.alves.b@gmail.com>
2019-10-14 13:41:44 -0300

    Add Constants file with api key variable

---
+++ TrendingNewsML/TrendingNewsML/Model/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  TrendingNewsML
+//
+//  Created by Jacqueline Alves on 10/10/19.
+//  Copyright © 2019 maqueline. All rights reserved.
+//
+
+let gnewsAPIKey = "YOUR API KEY"
